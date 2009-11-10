43daff5386c7373c0d552e524b86829a912bab58
Jonas <jvadev@gmail.com>
2009-11-10 15:33:37 +0100

    aufgabe a fertig

--- b2/plot.p
+++ b2/plot.p
@@ -3,4 +3,3 @@ set ylabel "MFlops"
 
 plot "plot.data" using 1:2 title 'gauss' with lines, \
    "plot.data" using 1:3 title 'gauss blocked'with lines
-      
--- b3/a2uebung8.c
+++ b3/a2uebung8.c
@@ -1,62 +1,67 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <iostream>
 #ifdef _OPENMP
   #include <omp.h>
 #endif
 
 int main(int argc, char** argv)
 {
-	int n;
-	for(n=1;n<4;n++){
-		int N=128*(int)pow(2,n);
-		int p;
-                int** mat1=(int **) malloc(sizeof(int*)*N);
-                int** mat2=(int **) malloc(sizeof(int*)*N);
-                int** mat3=(int **) malloc(sizeof(int*)*N);
-		for(p=0;p<N;p++){
-			mat1[p]=(int *) malloc(sizeof(int)*N);
-			mat2[p]=(int *) malloc(sizeof(int)*N);
-			mat3[p]=(int *) malloc(sizeof(int)*N);
-		}
-		int k,i,j,c,m;
-		double tall;
-		for(m=0;m<11;m++){
-			omp_set_num_threads((int)pow(2,m));
+  int n;
+  for(n=0;n<3;n++){
+    int N=128*(int)pow(2,n);
+    int p;
+    double** mat1=(double **) malloc(sizeof(double*)*N);
+    double** mat2=(double **) malloc(sizeof(double*)*N);
+    double** mat3=(double **) malloc(sizeof(double*)*N);
+    for(p=0;p<N;p++){
+      mat1[p]=(double *) malloc(sizeof(double)*N);
+      mat2[p]=(double *) malloc(sizeof(double)*N);
+      mat3[p]=(double *) malloc(sizeof(double)*N);
+    }
+    int k,i,j,c,m;
+    double tall;
+    for(m=0;m<11;m++){
+      omp_set_num_threads((int)pow(2,m));
+      tall = 0.0;
 
-			// Matrizen initialisieren
-			for(j=0;j<N;j++){
-				for(k=0;k<N;k++){
-					mat1[j][k]=k+j;
-					mat2[j][k]=k+j;
-					mat3[j][k]=k+j;
-				}
-			}
+      // Mehrfacher Schleifendurchlauf fuer gemitteltes Ergebnis
+      for(c=0;c<5;c++){
 
-			// Mehrfacher Schleifendurchlauf fuer gemitteltes Ergebnis
-			for(c=0;c<5;c++){
-
-				// Timer setzen
-				double tstart = omp_get_wtime();
-				// Matrizen multiplizieren
-				#pragma omp parallel for schedule (static)
-				for(i=0;i<N;i++){
-					for(j=0;j<N;j++){
-						for(k=0;k<N;k++){
-							mat3[i][j]+=mat1[i][k]*mat2[k][j];
-						}
-					}
-				}
-
-				//Timer stoppen und ausgeben
-				double tend = omp_get_wtime();
-				tall += tend-tstart;
-			}
-			printf("m: %d n: %d time: %f\n",(int)pow(2,m),128*(int)pow(2,n),tall/c);
-		}
-		free(mat1);
-		free(mat2);
-		free(mat3);
+	// Matrizen initialisieren
+	for(j=0;j<N;j++){
+	  for(k=0;k<N;k++){
+	    mat1[j][k]=(double)k+j;
+	    mat2[j][k]=(double)k+j;
+	    mat3[j][k]= 0.0;
+	  }
 	}
-	return 0;
+	
+	// Timer setzen
+	double tstart = omp_get_wtime();
+	// Matrizen multiplizieren
+#pragma omp parallel for schedule (static,8) private(j,k) shared(mat3,mat1,mat2)
+	for(i=0;i<N;i++){
+	  for(j=0;j<N;j++){
+	    for(k=0;k<N;k++){
+	      mat3[i][j]+=mat1[i][k]*mat2[k][j];
+	    }
+	  }
+	}
+
+	//Timer stoppen und ausgeben
+	double tend = omp_get_wtime();
+	tall += tend-tstart;
+      }
+      if(false)
+	printf("m: %d n: %d time: %f\n",(int)pow(2,m),128*(int)pow(2,n),tall/c);
+      else
+	printf("%d %4d %f\n",(int)pow(2,m),128*(int)pow(2,n),tall/c);
+    }
+    //    free(mat1);
+    //    free(mat2);
+    //    free(mat3);
+  }
+  return 0;
 }
