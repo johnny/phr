7f3eb97faef704eb30890e32ac5dccc4b86d2251
Thomas Monninger <thomas@Laptopb.Monninger>
2009-11-10 15:42:04 +0100

    Aufgabe b

---
+++ b3/a2uebung8_stat13.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#ifdef _OPENMP
+  #include <omp.h>
+#endif
+
+int main(int argc, char** argv)
+{
+		int N=500;
+		int p;
+                int** mat1=(int **) malloc(sizeof(int*)*N);
+                int** mat2=(int **) malloc(sizeof(int*)*N);
+                int** mat3=(int **) malloc(sizeof(int*)*N);
+		for(p=0;p<N;p++){
+			mat1[p]=(int *) malloc(sizeof(int)*N);
+			mat2[p]=(int *) malloc(sizeof(int)*N);
+			mat3[p]=(int *) malloc(sizeof(int)*N);
+		}
+		int c,m;
+		double tall;
+		for(m=1;m<=16;m*=2){
+			
+			omp_set_num_threads(m);
+			// Matrizen initialisieren
+			for(int j=0;j<N;j++){
+				for(int k=0;k<N;k++){
+					mat1[j][k]=k+j;
+					mat2[j][k]=k+j;
+					mat3[j][k]=k+j;
+				}
+			}
+
+			// Mehrfacher Schleifendurchlauf fuer gemitteltes Ergebnis
+			for(c=0;c<5;c++){
+				#pragma omp parallel
+				{
+				// Timer setzen
+				double tstart = omp_get_wtime();
+				// Matrizen multiplizieren
+				#pragma omp for schedule (static,13)
+				for(int i=0;i<N;i++){
+					for(int j=0;j<N;j++){
+						for(int k=0;k<N;k++){
+							mat3[i][j]+=mat1[i][k]*mat2[k][j];
+						}
+					}
+				}
+
+				//Timer stoppen und ausgeben
+				double tend = omp_get_wtime();
+				tall += tend-tstart;
+				}
+			}
+			printf("static,13 - m: %d n: %d time: %f\n",m,500,tall/c);
+			tall=0;
+		}
+		free(mat1);
+		free(mat2);
+		free(mat3);
+	return 0;
+}
